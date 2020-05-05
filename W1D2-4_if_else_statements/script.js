2d42e484e2b111af772bc15e271ca31b4eb7ce21
Mark Verbeek <57841157+markverbeek-amsterdam@users.noreply.github.com>
2020-05-05 14:35:06 +0200

    Add files via upload

---
+++ W1D2-4_if_else_statements/script.js
@@ -0,0 +1,23 @@
+const age = 45;
+
+if (age >= 18) {
+	console.log('Je mag naar binnen');
+} else {
+	console.log('Kom maar terug als je 18 bent');
+}
+
+const isFemale = false;
+
+if (isFemale) {
+	console.log('Hartelijk Welkom!');
+} else {
+	console.log('Heute Nicht');
+}
+
+const driverStatus = 'bob';
+
+if (driverStatus == 'bob') {
+	console.log('Je bent bob en mag rijden');
+} else {
+	console.log('Je mag niet rijden');
+}
